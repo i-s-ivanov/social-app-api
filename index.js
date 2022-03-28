8b2c4ec95c519a00b16e2e0af428556c5ef10ab3
Iskren Ivanov <pecito_vn@hotmail.com>
2022-03-28 20:35:53 +0300

    Post model
    Post model added

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
-const userAuth = require("./routes/auth")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 
 dotenv.config();
 
@@ -22,7 +23,8 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute)
-app.use("/api/auth", userAuth)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
 
 app.get("/", (req,res) => {
     res.send("Welcome to homepage")
